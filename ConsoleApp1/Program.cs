0338d6501be87aa4ff892b213788947ae9894d55
Ianpougher <ianpougher@gmail.com>
2019-05-19 12:12:35 -0600

    adds first test files, and creates udp write message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,39 +9,66 @@ namespace ConsoleApp1
     {
         // sends a write message containing, the opcode W
         // the filename, the location/offset, and the data to be written 
+        //TODO: Form a voting system, as well as creating a repeating system if the write send fails 
+        //TODO: FIND APPROPRIATE FILLER WHEN BYTECODE IS SHORTER THAN SET LENGTH 
         static void SendWrite(string filename, int location, byte[] data )
         {
-            byte opcode  = (byte)'W';
+            Byte opcode  = (byte)'W';
+            Byte[] filecode = System.Text.Encoding.ASCII.GetBytes(filename);
+            Byte[] locationcode = BitConverter.GetBytes(location);
+            Byte length = (byte)data.Length;
+            Console.WriteLine(locationcode.Length);
+            Byte []sendarray = new Byte[48];
+            sendarray[0] = opcode;
+            filecode.CopyTo(sendarray,1);
+            locationcode.CopyTo(sendarray,33);
+            sendarray[38] = length;
+            data.CopyTo(sendarray, 39);
             
+
+            return;
+
         }
         // sends a read message containing, the opcode R
         // the filename,and the location/offset
+        //TODO create a repeat system that recalls itself if the read send fails 
         void SendRead(string filename, int location)
         {
+            Byte opcode  = (byte)'R';
+            Byte[] filecode = System.Text.Encoding.ASCII.GetBytes(filename);
+            Byte[] locationcode = BitConverter.GetBytes(location);
             
         }
-        //breaks apart a file and sends the correct chunk specified by the offset/location given 
-        //TODO change this to a full file chunking system, then feed into the sendWrite function one piece at a time 
+        //breaks apart a file and sends the correct chunk specified by the offset/location given
         public static void FileChunk(string path)
         {
             byte [] file_bytes = File.ReadAllBytes(path);
+            Console.WriteLine(file_bytes.Length);
             for (int i = 0; i <= file_bytes.Length; i+=10)
             {
                 byte [] sendchunk  = new byte[10];
                 for (int j = i; j <= (i + 10); j++)
                 {
-                    Console.WriteLine(file_bytes[j]);
+                    //Console.WriteLine(file_bytes[j]);
+                    //THIS MAYBE AN ISSUE IF THE LAST BIT DOES NOT APPEAR
+                    // CHANGE THIS TO THE LENGTH OF THE ARRAY IN THAT CASE 
+                    if (j >= file_bytes.Length - 1)
+                    {
 
-                    sendchunk.Append(file_bytes[j]);
-                    
-                    
+                    }
+                    else
+                    {
+                        sendchunk.Append(file_bytes[j]);
+                    }
+
                 }
 
                 SendWrite(path, i, sendchunk);
 
             }
         }
-
+//may want to change the main to just accept the filename and milliseconds between corrections
+// this would automate the read and write, and just ensure that the files are correct every n milliseconds 
         static void Main(string[] args)
         {
             Console.WriteLine(args.Length);
@@ -52,6 +79,17 @@ namespace ConsoleApp1
 
             }
             if (args[0] == "Write")
+            {
+                if (args.Length == 1)
+                {
+                    Console.WriteLine("you need a filepath to run this command");
+                }
+                else if (args.Length == 2)
+                {
+                    FileChunk(args[1]);
+                }
+            }
+            else if (args[0] == "Read")
             {
                 if (args.Length == 1)
                 {
