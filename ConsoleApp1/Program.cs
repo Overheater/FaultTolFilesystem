e01681568532d48836a0eba39fdf91df7c72c5f2
Ianpougher <ianpougher@gmail.com>
2019-05-22 12:39:00 -0600

    adds skeleton for copy amount choice

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,27 +10,50 @@ namespace ConsoleApp1
     {
         // sends a write message containing, the opcode W
         // the filename, the location/offset, and the data to be written 
-        //TODO: Form a voting system, as well as creating a repeating system if the write send fails 
         //TODO: FIND APPROPRIATE FILLER WHEN BYTECODE IS SHORTER THAN SET LENGTH 
-        private static void SendWrite(string filename, int location, byte[] data)
+        private static void SendWrite(string filename, int location, byte[] data,bool extracopy)
         {
             var opcode = (byte) 'W';
-            var filecode = Encoding.ASCII.GetBytes(filename);
             var locationcode = BitConverter.GetBytes(location);
             var length = (byte) data.Length;
             Console.WriteLine(locationcode.Length);
-            var sendarray = new byte[48];
-            sendarray[0] = opcode;
-            filecode.CopyTo(sendarray, 1);
-            locationcode.CopyTo(sendarray, 33);
-            sendarray[37] = length;
-            data.CopyTo(sendarray, 38);
-            udpsend(sendarray, 'S');
+            if (extracopy == false)
+            {
+                for (int i = 0; i<3;i++ )
+                {
+                    filename = i.ToString() + filename;
+                    Console.WriteLine(filename);
+                    var filecode = Encoding.ASCII.GetBytes(filename);
+                    var sendarray = new byte[48];
+                    sendarray[0] = opcode;
+                    filecode.CopyTo(sendarray, 1);
+                    locationcode.CopyTo(sendarray, 33);
+                    sendarray[37] = length;
+                    data.CopyTo(sendarray, 38);
+                    Udpsend(sendarray, 'S');
+                }
+            }
+            // ReSharper disable once RedundantBoolCompare
+            if (extracopy == true)
+            {
+             for (int i = 0; i<6;i++ )
+             {
+                var filecode = Encoding.ASCII.GetBytes(filename);
+                var sendarray = new byte[48];
+                sendarray[0] = opcode;
+                filecode.CopyTo(sendarray, 1);
+                locationcode.CopyTo(sendarray, 33);
+                sendarray[37] = length;
+                data.CopyTo(sendarray, 38);
+                Udpsend(sendarray, 'S');
+             }
+            }
         }
 
         // sends a read message containing, the opcode R
         // the filename,and the location/offset
-        //TODO create a repeat system that recalls itself if the read send fails 
+        //TODO create a repeat system that recalls itself if the read send fails
+        //TODO: Form a voting system, as well as creating a repeating system if the write send fails 
         private void SendRead(string filename, int location)
         {
             var opcode = (byte) 'R';
@@ -40,35 +63,44 @@ namespace ConsoleApp1
             sendarray[0] = opcode;
             filecode.CopyTo(sendarray,1);
             locationcode.CopyTo(sendarray,33);
-            udpsend(sendarray,'R');
+            Udpsend(sendarray,'R');
 
         }
 
         //breaks apart a file and sends the correct chunk specified by the offset/location given
-        private static void FileChunk(string path)
+        private static void FileChunk(string path,bool extracopy)
         {
-            var file_bytes = File.ReadAllBytes(path);
-            Console.WriteLine(file_bytes.Length);
-            for (var i = 0; i <= file_bytes.Length; i += 10)
+            byte [] fileBytes = File.ReadAllBytes(path);
+            Console.WriteLine(fileBytes.Length);
+            int size = fileBytes.Length;
+            for (var i = 0; i <= fileBytes.Length; i += 10)
             {
                 var sendchunk = new byte[10];
                 for (var j = i; j <= i + 9; j++)
                     //Console.WriteLine(file_bytes[j]);
                     //THIS MAYBE AN ISSUE IF THE LAST BIT DOES NOT APPEAR
                     // CHANGE THIS TO THE LENGTH OF THE ARRAY IN THAT CASE 
-                    if (j >= file_bytes.Length - 1)
+                    if (j >= fileBytes.Length - 1)
                     {
                     }
                     else
                     {
-                        sendchunk[j - i] = file_bytes[j];
+                        sendchunk[j - i] = fileBytes[j];
                     }
 
-                SendWrite(path, i, sendchunk);
+                if (extracopy == true)
+                {
+                    SendWrite(path, i, sendchunk,true);
+                }
+                else if (extracopy == false)
+                {
+                    SendWrite(path, i, sendchunk,false);
+                }
+
             }
         }
         //TODO: add repeated function system using the functype variable to correctly resend  
-        private static void udpsend(byte[] sendarray, char functype)
+        private static void Udpsend(byte[] sendarray, char functype)
         {
             var successful = false;
             var sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
@@ -81,24 +113,37 @@ namespace ConsoleApp1
 // this would automate the read and write, and just ensure that the files are correct every n milliseconds 
         private static void Main(string[] args)
         {
-            Console.WriteLine(args.Length);
-            if (args.Length == 0)
+            if (args.Length <2)
             {
-                Console.WriteLine("Please enter a an argument.");
+                Console.WriteLine("Please enter a complete argument. Accepted form is <program> <file name> <extra copies?: -Y or -N>");
                 return;
             }
-
-            if (args[0] == "Write")
+            else if (args.Length >2)
             {
-                if (args.Length == 1)
-                    Console.WriteLine("you need a filepath to run this command");
-                else if (args.Length == 2) FileChunk(args[1]);
+                Console.WriteLine("Please enter only 2 accepted arguments. Accepted form is <program> <file name> <extra copies?: -Y or -N>");
+                return;
             }
-            else if (args[0] == "Read")
+            else if (args.Length ==2)
             {
-                if (args.Length == 1)
-                    Console.WriteLine("you need a filepath to run this command");
-                else if (args.Length == 2) FileChunk(args[1]);
+                if (args[1] == "-N")
+                {
+                    FileChunk(args[0],false);
+                    while (true)
+                    {
+                    
+                    }
+                }
+                else if (args[1] == "-Y")
+                {
+                    FileChunk(args[0],true);
+                    while (true)
+                    {
+                    
+                    }
+
+                    return;
+                }
+
             }
         }
     }
